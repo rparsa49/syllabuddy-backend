1e155cb2868b2c6a0df3a385cf247bdb1c60bb4b
roya parsa <99053997+rparsa49@users.noreply.github.com>
2023-10-25 08:24:03 -0400

    created requirements.txt and made template registration endpoint

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Flask!'
 
+# endpoint for user registration
+@app.route('/register', methods=['POST'])
+def register_user():
+    data = request.get_json()
+    return jsonify({'message': 'User registered successfully'})
+
 if __name__ == '__main__':
     app.run()
