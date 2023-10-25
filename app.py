50277ea4835b47ffd635904f6e7900358837f499
roya parsa <99053997+rparsa49@users.noreply.github.com>
2023-10-25 16:18:49 -0400

    deleted reqs.txt and replaced pyodbc with mysqlconnector

--- app.py
+++ app.py
@@ -1,30 +1,29 @@
 from flask import Flask, request, jsonify
-import pyodbc
+import mysql.connector
 
 app = Flask(__name__)
 
-# Configure your database connection (ADD ACTUAL DATABASE INFO LATER)
-db_connection = pyodbc.connect(
-    "Driver={SQL Server};"
-    "Server=your_server;"
-    "Database=your_database;"
-    "UID=your_username;"
-    "PWD=your_password;"
+# Configure MySQL database connection
+db_connection = mysql.connector.connect(
+    host='167.206.126.218',
+    user='syllabuddy-user',  
+    password='password', 
+    database='your_database'  # Replace with the name of your MySQL database
 )
 
-# Endpoint for user registration (FIX TABLE NAMES LATER)
+# Endpoint for user registration (FIX DB/TABLE NAMES LATER)
 @app.route('/register', methods=['POST'])
 def register_user():
     data = request.get_json()
     cursor = db_connection.cursor()
-    
+
     # Define the SQL query to insert user data
-    insert_query = "INSERT INTO Users (email, password, role, university) VALUES (?, ?, ?, ?)"
-    
+    insert_query = "INSERT INTO Users (email, password, role, university) VALUES (%s, %s, %s, %s)"
+
     try:
         # Execute the SQL query with user data
         cursor.execute(insert_query, (data['email'], data['password'], data['role'], data['university']))
-        db_connection.commit()  # Commit the changes to the database
+        db_connection.commit()  
         cursor.close()
         return jsonify({'message': 'User registered successfully'})
     except Exception as e:
